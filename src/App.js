3e2c93f2d88d6ef5b3ba04c02dda2357ccde3147
LAPTOP-O3MJRTK1\ashle <ashley.aditya@gmail.com>
2022-05-25 12:02:12 +1000

    advice api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+  const fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then(({ data }) => setAdvice(data.slip.advice));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{advice}</h1>
+      <button onClick={fetchAdvice}> getAdvice</button>
     </div>
   );
 }
